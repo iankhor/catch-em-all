eea4a2c97e280bc551e066625cbaa3d807b1bb93
iankhor <iankhorbc.thecoder@gmail.com>
2022-06-01 11:33:16 +1000

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import * as Pokedex from 'pokeapi-js-wrapper'
+
+const P = new Pokedex.Pokedex()
+
+const fetch = async () => {
+  const list = await P.getPokemonsList()
+  console.log(list)
+}
 
 function App() {
+  useEffect(() => {
+    fetch()
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello world
     </div>
   );
 }
